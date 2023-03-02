fed15e510dad638c5e5705f261507a308a9b70e4
Diyora Akhmedbekova <ahmedbekovadiera7919@gmail.com>
2023-03-02 16:30:55 +0500

    hometask_5

--- Homeworks/homework5/Program.cs
+++ Homeworks/homework5/Program.cs
@@ -57,25 +57,70 @@
 
 // [-4, -6, 89, 6] -> 0
 
-int[] CreateRandomArray(int size, int minValue, int maxValue)
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] numbers = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         numbers[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return numbers;
+// }
+// void SumOfOddNumbers (int[] numbers, int size){
+//     int sum = 0;
+//     for (int i = 0; i < size; i++){
+//         if (!(i %2 == 0)){
+//             sum+=numbers[i];
+//         }
+//         Console.Write(numbers[i] + " ");
+//     }
+// Console.WriteLine();
+// Console.WriteLine($"sum odd numbers of massiv -> {sum}");    
+// }
+
+// Console.WriteLine("Input length of massiv: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input min number: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max number: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+// SumOfOddNumbers(CreateRandomArray(size, minValue, maxValue), size);
+
+//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и 
+//минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] CreateRandomArray(int size, int minValue, int maxValue)
 {
-    int[] numbers = new int[size];
+    double[] numbers = new double[size];
     for (int i = 0; i < size; i++)
     {
         numbers[i] = new Random().Next(minValue, maxValue + 1);
     }
     return numbers;
 }
-void SumOfOddNumbers (int[] numbers, int size){
-    int sum = 0;
+
+void DifferenceMaxMin (double[] numbers, int size){
+    double max = numbers[0];
+    double min = numbers[0];
+    int maxValue = 0;
+    int minValue = 0;
+    double difference = 0;
     for (int i = 0; i < size; i++){
-        if (!(numbers[i] %2 == 0)){
-            sum+=numbers[i];
+        if (numbers[i] > max){
+            max = numbers[i];
+        }
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
         }
         Console.Write(numbers[i] + " ");
     }
+    difference = max-min;
 Console.WriteLine();
-Console.WriteLine($"sum odd numbers of massiv -> {sum}");    
+Console.WriteLine($"difference between max and min number -> {difference}");    
 }
 
 Console.WriteLine("Input length of massiv: ");
@@ -84,4 +129,4 @@ Console.WriteLine("Input min number: ");
 int minValue = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input max number: ");
 int maxValue = Convert.ToInt32(Console.ReadLine());
-SumOfOddNumbers(CreateRandomArray(size, minValue, maxValue), size);
+DifferenceMaxMin(CreateRandomArray(size, minValue, maxValue), size);
